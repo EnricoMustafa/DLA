1927336e8556d5abeacee67150347f1bd5c4769e
enrico brasil <enrico.brasil@prit.app>
2025-04-06 16:35:32 -0300

    subtracao check

---
+++ DIA2/subtracao.js
@@ -0,0 +1,40 @@
+console.log("------------------------- S U B T R A Ç Ã O ------------------------- \n")
+
+// compras cartão de credito
+
+let camiseta = 250;
+let lanches = 80;
+let compraE = 200;
+
+let total = camiseta + lanches + compraE; 
+let totalFinal = camiseta + lanches - compraE; 
+
+
+console.log("A fatura desse mes foi de: R$" + total +",00");
+console.log("A fatura desse mes com o estorno foi: R$" + totalFinal +",00");
+
+console.log("----------------------------------------------------- \n");
+
+// Calculo de idade com os ano atual 
+
+const anoAtual = 2025;
+let anoNascimento = 2021;
+let idadeAtual = anoAtual - anoNascimento;
+
+console.log("A sua idade esse ano é: " +idadeAtual);
+
+console.log("----------------------------------------------------- \n");
+
+//total de moedas de um jogo
+
+let totalMoedas = 5000;
+let item1 = 240;
+let item2 = 500;
+
+let compraItems = totalMoedas - (item1 + item2);
+
+console.log("O total de moedas apos comprar os items é de: " + compraItems);
+
+
+console.log("----------------------------------------------------- \n");
+
