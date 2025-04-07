c7bb83d1163e4b06eba88921238bfa5aa4fae815
enrico brasil <enrico.brasil@prit.app>
2025-04-07 11:52:55 -0300

    performanceAluno

---
+++ DIA4/performanceAluno.js
@@ -0,0 +1,20 @@
+//Performance do aluno
+
+let nota = 1;
+
+if(nota < 5){
+    console.log("Nota: " +nota);
+    console.log("Situação: insuficiente");
+}else if(nota <= 6){
+    console.log("Nota: " +nota);
+    console.log("Situação: regular");
+}else if(nota <= 7.5){
+    console.log("Nota: " +nota);
+    console.log("Situação: Boa");
+}else if(nota < 9){
+    console.log("Nota: " +nota);
+    console.log("Situação: Muito Bom");
+}else{
+    console.log("Nota: " +nota);
+    console.log("Situação: Excelente");
+}
