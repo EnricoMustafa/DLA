6a65ae653164d2309595315cffed66a0061e3cf0
enrico brasil <enrico.brasil@prit.app>
2025-04-08 10:53:16 -0300

    melhorias

--- DIA5/lancamentoFoguete.js
+++ DIA5/lancamentoFoguete.js
@@ -1,13 +1,10 @@
 //Contagem regressiva para lançamento de foguete
 
-let contagem = 10;
-
-for(i = contagem; i >= 0; i--){
-    if(i < 3){
-        console.log(" ATENÇÂO ");
-    }if(i <= 0) {
-        console.log("Lançamento do foguete");
+for(let i = 10; i >= 0; i--){
+    if(i <= 3){
+        console.log(" ATENÇÂO " + i);
+    }else{
+        console.log(i);
     }
-
-    console.log(i)
 }
+console.log("Lançamento foguete");
