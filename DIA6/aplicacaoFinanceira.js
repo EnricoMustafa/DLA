47f34109718998964035372c18ef5402e2b3031f
enrico brasil <enrico.brasil@prit.app>
2025-04-09 08:59:28 -0300

    outra forma

--- DIA6/aplicacaoFinanceira.js
+++ DIA6/aplicacaoFinanceira.js
@@ -1,8 +1,19 @@
 // Aplicação Financeira
 
+// const aplicacaoFinanceira = (investimento, taxaJuros, anosInvestidos) =>{
+//     for(let i = 1; i<=anosInvestidos; i++){
+//         investimento += investimento * taxaJuros;
+//         console.log("Ano: " +i +" | R$" +investimento.toFixed(2));
+//     }
+// }
+
+// aplicacaoFinanceira(1000,0.12,10);
+
+// aplicacaoFinanceira(1000);
+
 const aplicacaoFinanceira = (investimento) =>{
     let anoAtual = 2025;
-    for(i = anoAtual; i<=2035; i++){
+    for(let i = anoAtual; i < 2035; i++){
         investimento = investimento * 1.12;
         console.log("Ano: " +i +" | R$" +investimento.toFixed(2));
     }
