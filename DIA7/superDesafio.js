e5275426dcc90397ab22c6076802e6d60a348a32
enrico brasil <enrico.brasil@prit.app>
2025-04-11 00:24:14 -0300

    função de debito e correção de deposito

--- DIA7/superDesafio.js
+++ DIA7/superDesafio.js
@@ -5,67 +5,40 @@ let limite = 0.10;
 let totalContas = conta1 + conta2; //quando o saldo das duas contas chegar a 1000 o limite vira 1.1
 let limiteContas = 0;
 
-// CORRETO
+// CORRETO FAZER UM VERIFICADOR SE O SALDO É 0
 const calcularSaldoDasContas = (conta) => {
-    let saldoTotal = conta1+conta2;
-
-    if(conta == 1){
-        console.log("Saldo conta1: R$" +conta1);
-    } else if(conta == 2){
-        console.log("Saldo conta2: R$" +conta2);
-    } else if(conta == 3){
-        console.log("O saldo total das contas é: R$" +saldoTotal)
-    } else{
-        console.log("Digite uma conta valida");
-    }
-    
-}
-
-// //ERRO
-// const verificarSaldo = (contaOrigem) => {
-//     if(contaOrigem == 1){
-//         if(conta1 != 0){
-//             calcularSaldoDasContas(1);
-//         } else {
-//             console.log("Saldo insuficiente, saldo atual da conta1: R$" +conta1);
-//         }
-//     }
-
-//     if(contaOrigem == 2){
-//         if(conta2 <= 0){
-//             console.log("Saldo insuficiente, saldo atual da conta1: R$" +conta2);
-//         } else if(conta2 > 1000){
-//             conta2 = conta2 / limite;
-//             console.log("A conta está usando o limite a cima de R$1000,00");
-//         }
-//     }
- 
-// }
+    let saldoTotal = conta1 + conta2;
+        if (conta == 1) {
+            console.log("Saldo conta1: R$" + conta1);
+        } else if (conta == 2) {
+            console.log("Saldo conta2: R$" + conta2);
+        } else if (conta == 3) {
+            console.log("O saldo total das contas é: R$" + saldoTotal)
+        } else {
+            console.log("Digite uma conta valida");
+        }
+    } 
 
 //CORRETO
 const fazerDeposito = (contaOrigem, valor) => {
-    if(contaOrigem == 1){
+    if (contaOrigem == 1) {
         conta1 = conta1 + valor;
-        calcularSaldoDasContas(1);
-    } else if(contaOrigem == 2){
+        console.log("DEPOSITO EFETUADO COM SUCESSO NA conta1")
+    } else if (contaOrigem == 2) {
         conta2 = conta2 + valor;
-        calcularSaldoDasContas(2);
+        console.log("DEPOSITO EFETUADO COM SUCESSO NA conta2")
     }
 };
 
-const fazerDebito = (opcao, valorDebito) => {
-    switch (opcao) {
-        case 1:
-            conta1 = conta1 - valorDebito
-            console.log("Foi debitado R$" + valorDebito + ",00 na conta1 | O saldo atual é de: R$" + conta1);
-            break;
-        case 2:
-            conta2 = conta2 - valorDebito
-            console.log("Foi debitado R$" + valorDebito + ",00 na conta2 | O saldo atual é de: R$" + conta2);
-            break;
-
-        default:            console.log("Erro no debito")
-            break;
+const fazerDebito = (contaOrigem, valorDebito) => {
+    if(contaOrigem == 1){
+        conta1 = conta1 - valorDebito;
+        console.log("Debito de R$" +valorDebito)
+    } else if(contaOrigem == 2){
+        conta2 = conta2 - valorDebito;
+        console.log("Debito de R$" +valorDebito)
+    } else{
+        console.log("ERRO AO EFETUAR O DEBITO")
     }
 }
 
@@ -98,3 +71,7 @@ const tranferirSaldo = (opcao, valorTranferencia) => {
     }
 }
 
+fazerDeposito(1, 200);
+calcularSaldoDasContas(1);
+fazerDebito(1,100);
+calcularSaldoDasContas(1);
