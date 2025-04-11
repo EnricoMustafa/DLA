e148450d75ba56be9e190787cb809c6b0c3e10a5
enrico brasil <enrico.brasil@prit.app>
2025-04-11 00:53:15 -0300

    dolar real

--- DIA7/superDesafio.js
+++ DIA7/superDesafio.js
@@ -4,16 +4,17 @@ let conta2 = 0;
 let limite = 0.10;
 let totalContas = conta1 + conta2; //quando o saldo das duas contas chegar a 1000 o limite vira 1.1
 let limiteContas = 0;
+let cotacaoDolar = 5.10;
 
 // CORRETO FAZER UM VERIFICADOR SE O SALDO É 0
 const calcularSaldoDasContas = (conta) => {
     let saldoTotal = conta1 + conta2;
         if (conta == 1) {
-            console.log("Saldo conta1: R$" + conta1);
+            console.log("Saldo conta1: R$" + conta1.toFixed(2));
         } else if (conta == 2) {
-            console.log("Saldo conta2: R$" + conta2);
+            console.log("Saldo conta2: R$" + conta2.toFixed(2));
         } else if (conta == 3) {
-            console.log("O saldo total das contas é: R$" + saldoTotal)
+            console.log("O saldo total das contas é: R$" + saldoTotal.toFixed(2))
         } else {
             console.log("Digite uma conta valida");
         }
@@ -30,6 +31,7 @@ const fazerDeposito = (contaOrigem, valor) => {
     }
 };
 
+//CORRETO
 const fazerDebito = (contaOrigem, valorDebito) => {
     if(contaOrigem == 1){
         conta1 = conta1 - valorDebito;
@@ -42,36 +44,37 @@ const fazerDebito = (contaOrigem, valorDebito) => {
     }
 }
 
-const tranferirSaldo = (opcao, valorTranferencia) => {
-    switch (opcao) {
-        case 1:
-            if (conta1 > valorTranferencia) {
-                conta1 = conta1 - valorTranferencia;
-                conta2 = conta2 + valorTranferencia;
-                console.log("O valor  transferido da conta1 foi " + valorTranferencia + " Saldo atual: " + conta1);
-            } else {
-                console.log("ATENÇÂO SEM SALDO SUFICIENTE NA CONTA1")
-
-            }
-            break;
-        case 2:
-            if (conta2 > valorTranferencia) {
-
-                conta2 = conta2 - valorTranferencia;
-                conta1 = conta1 + valorTranferencia;
-                console.log("O valor  transferido da conta2 foi " + valorTranferencia + " Saldo atual: " + conta2);
-            } else {
-                console.log("ATENÇÂO SEM SALDO SUFICIENTE NA CONTA2")
-            }
-            break;
-
-        default:
-            console.log("Erro na tranferencia")
-            break;
+//CORRETO
+const tranferirSaldo = (contaOrigem, valorTranferencia) => {
+    if(contaOrigem == 1){
+        conta1 = conta1 - valorTranferencia;
+        conta2 = valorTranferencia + conta2;
+        console.log("Transferencia efetuada com sucesso") 
+    } else if(contaOrigem == 2){
+        conta2 = conta2 - valorTranferencia;
+        conta1 = valorTranferencia + conta1;
+        console.log("Transferencia efetuada com sucesso") 
     }
 }
 
-fazerDeposito(1, 200);
-calcularSaldoDasContas(1);
-fazerDebito(1,100);
-calcularSaldoDasContas(1);
+const converterDolarReal = (contaOrigem, valor) => {
+    if(contaOrigem == 1){
+        valorConversao = valor/cotacaoDolar;
+        conta1 = conta1 - valor;
+        console.log("O valor colocado foi: R$" +valor + " e em dollar ficou: US$" +valorConversao.toFixed(2))
+    } else if(contaOrigem == 2){
+        valorConversao = valor/cotacaoDolar;
+        conta2 = conta2 - valor;
+        console.log("O valor colocado foi: R$" +valor + " e em dollar ficou: US$" +valorConversao.toFixed(2))
+    }
+}
+
+fazerDeposito(1, 500);
+fazerDeposito(2, 300);
+calcularSaldoDasContas(2)
+tranferirSaldo(1,200);
+calcularSaldoDasContas(2)
+calcularSaldoDasContas(1)
+
+converterDolarReal(1,100);
+calcularSaldoDasContas(1)
