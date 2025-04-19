ed04f6fa7520f35746cfb328c2dc24cc1921bdb1
enrico brasil <enrico.brasil@prit.app>
2025-04-18 21:59:07 -0300

    Super deafio

--- DIA14/superDesafio.js
+++ DIA14/superDesafio.js
@@ -12,50 +12,57 @@ let filasCaixas = {
     caixa10: []
 }
 
-function entrarNaFila(caixa,nomeCliente){
-    if(filasCaixas[caixa]){
-        filasCaixas[caixa][filasCaixas[caixa].lenght] = nomeCliente;
-        console.log(`O ${nomeCliente} entrou na fila do ${caixa}`);
-    } else{
+function entrarNaFila(caixa, nome) {
+    if (filasCaixas[caixa]) {
+        filasCaixas[caixa][filasCaixas[caixa].length] = nome; 
+        console.log(`${nome} entrou na fila do ${caixa}.`);
+    } else {
+        console.log(`O ${caixa} não existe.`);
+    }
+}
+
+function foiAtendido(caixa) {
+    if (filasCaixas[caixa] && filasCaixas[caixa].length > 0){
+        let clienteAtendido = filasCaixas[caixa][0];
+        for (let i = 1; i < filasCaixas[caixa].length; i++){
+            filasCaixas[caixa][i-1] = filasCaixas[caixa][i]
+        }
+        filasCaixas[caixa].length -= 1;
+        console.log(`${clienteAtendido} foi atendido no ${caixa}`)
+    } else if (filasCaixas[caixa]){
+        console.log(`A fila do ${caixa} está vazia`);
+    } else {
         console.log(`O ${caixa} não existe`);
     }
 }
 
-function foiAtendido(caixa){
-    
-    if(caixa == 1){
-        caixa1.shift()
-        console.log(`O cliente foi atendido no caixa: caixa ${caixa}`);
-        console.log(caixa1);
-    }else if(caixa == 2){
-        caixa2.shift()
-        console.log(`O cliente foi atendido no caixa: caixa ${caixa}`);
-        console.log(caixa2);
-    }else if(caixa == 3){
-        caixa3.shift()
-        console.log(`O cliente foi atendido no caixa: caixa ${caixa}`);
-        console.log(caixa3);
-    }else if(caixa == 4){
-        caixa4.shift() 
-        console.log(`O cliente foi atendido no caixa: caixa ${caixa}`);
-        console.log(caixa4);
-    }else if(caixa == 5 ){
-        caixa5.shift()
-        console.log(`O cliente foi atendido no caixa: caixa ${caixa}`);
-        console.log(caixa5);
-    }else{
-        console.log("Esse caixa não existe");
-    }
-
-}
-
 // o in no for serve para percorrer um objeto
-function mostrarCaixa(){
-    for(let caixas in filasCaixas){
-        console.log(caixas);
-        console.log(filasCaixas[caixas])
+function mostrarFilas(){
+    console.log("\nEstado das filas: ");
+    for (let i in filasCaixas) {
+        if (filasCaixas[i].length > 0) {
+            console.log(`${i}: ${filasCaixas[i].join(', ')}`)
+        }
     }
 }
 
 entrarNaFila("caixa2","Enrico");
-mostrarCaixa()
+entrarNaFila("caixa2","Mariana");
+entrarNaFila("caixa9","luisa");
+entrarNaFila("caixa10","bianca");
+entrarNaFila("caixa3","ikon");
+entrarNaFila("caixa2","cheila");
+entrarNaFila("caixa1","Pedro");
+entrarNaFila("caixa1","João");
+entrarNaFila("caixa4","Paulo");
+entrarNaFila("caixa2","Antopnio");
+entrarNaFila("caixa7","Guilherme");
+
+mostrarFilas();
+
+foiAtendido("caixa7");
+foiAtendido("caixa2");
+foiAtendido("caixa4");
+foiAtendido("caixa2");
+
+mostrarFilas();
