d29227fb8e0c7d1929d0d0413f2db760ae472740
enrico brasil <enrico.brasil@prit.app>
2025-04-22 15:21:48 -0300

    objetos literais

--- DIA15/desafioTrem.js
+++ DIA15/desafioTrem.js
@@ -1,1 +1,67 @@
 //Desafio do trem
+
+function criarNo(elemento){
+    return{
+        data: elemento,
+        next: null,
+    };
+}
+
+let listaEncadeada = {
+    head: null,
+
+    insertFirst: function (elemento) {
+        const novoNo = criarNo(elemento);
+
+        if(!this.head){
+            this.head = novoNo;
+        } else {
+            novoNo.next = this.head;
+            this.head = novoNo;
+        }
+
+        return elemento;
+    },
+
+    insertLast: function (elemento) {
+        if (!this.head) {
+            this.head = novoNo;
+        } else {
+            let atual = this.head;
+            while (atual.next) {
+                atual = atual.next;
+            }
+            atual.next = novoNo;
+        }
+        return elemento;
+    },
+
+    insertAt: function(elemento, posicao) {
+        const novoNo = criarNo(elemento);
+
+        if (posicao === 0) {
+            novoNo.next = this.head;
+            this.head = novoNo;
+            return elemento;
+        }
+
+        let atual = this.head;
+        let anterior = null;
+        let contador = 0;
+
+        while (contador < posicao && atual) {
+            anterior = atual;
+            atual = atual.next;
+            contador++;
+        }
+
+        if (contador === posicao) {
+            
+        }
+    }
+
+
+};
+
+console.log(listaEncadeada.insertFirst('Locomotiva'));
+console.log(listaEncadeada.insertLast(''));
