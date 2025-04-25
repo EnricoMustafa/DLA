540ac04aee1ad0ef5f7be3e4c335268f314faf16
enrico brasil <enrico.brasil@prit.app>
2025-04-24 22:58:01 -0300

    refactor playlist

--- DIA16/playlistComObjetoLiteral.js
+++ DIA16/playlistComObjetoLiteral.js
@@ -1,4 +1,4 @@
-function criarMusica(nome, artista, tempo){
+function criarMusica(nome, artista, tempo) {
     return {
         nome: nome,
         artista: artista,
@@ -12,13 +12,68 @@ const playlist = {
 
     adicionarMusica: function (nome, artista, tempo) {
         const novaMusica = criarMusica(nome, artista, tempo);
-        for (let i = this.musicas.length; i > 0; i--){
-            this.musicas[i] = this.musicas[i-1];
-        }
+        for (let i = this.musicas.length; i > 0; i--) {
+            this.musicas[i] = this.musicas[i - 1];
+        };
         this.musicas[0] = novaMusica;
         console.log(`Musica ${nome} adicionada a playlist`);
-    }
-}
+    },
 
-playlist.adicionarMusica("Esse cara sou eu","Roberto carlos", "5min");
-console.log(playlist.musicas)
+    removerMusica: function (posicao, quantidade) {
+        if (this.musicas.length === 0) {
+            console.log(`Playlist vazia`)
+        } else {
+            let removidas = this.musicas.splice(posicao, quantidade);
+            removidas.forEach((musica, index) => {
+                console.log(`\n${index + 1}. Música removida com sucesso!`);
+                console.log(`Artista: ${musica.nome}`);
+                console.log(`Nome da música: ${musica.artista}`);
+                console.log(`Duração: ${musica.tempo}`);
+            })
+        }
+    },
+
+    mostrarPlaylist: function () {
+        if (this.musicas.length === 0) {
+            console.log("Não existe Musica")
+        } else {
+            console.log(`Playlist:`);
+            this.musicas.forEach((musica, index) => {
+                console.log(`${index + 1}. ${musica.nome} - ${musica.artista} - ${musica.tempo}`);
+            })
+        }
+    },
+
+    tocarPLaylistToda: function () {
+        if (this.musicas.length === 0) {
+            console.log("A playlist está vazia");
+        } else {
+            for (let i = 0; i <= this.musicas.length; i++) {
+                console.log(`\n${i+1}. Iniciou Musica`);
+                console.log(`Finalizou Musica`);
+            }
+        }
+    },
+
+    tocarUmaMusica: function (posicao) {
+        if (posicao < 0 || posicao >= playlist.length) {
+            console.log("Posição inválida.");
+        } else {
+            let musica = this.musicas[posicao];
+            console.log(`\nMúsica selecionada para tocar:`);
+            console.log(`Artista: ${musica.nome}`);
+            console.log(`Nome da música: ${musica.artista}`);
+            console.log(`Duração: ${musica.tempo}`);
+        }
+    }
+
+};
+
+console.log(playlist.adicionarMusica("Judia de mim", "Roberto Carlos", "3 min"));
+console.log(playlist.adicionarMusica("Bota fofo", "Xheila carvalho", "2 min"));
+console.log(playlist.adicionarMusica("Adulterio", "Mr catra", "5 min"));
+
+console.log(playlist.mostrarPlaylist());
+console.log(playlist.removerMusica(1,1));
+console.log(playlist.tocarUmaMusica(1));
+playlist.tocarPLaylistToda();
